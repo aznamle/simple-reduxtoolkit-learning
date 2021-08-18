cba0c9d94c43f05e0fa3c365c0f823901573f0a4
aznamle <namle1998@hotmail.com>
2021-08-18 13:56:55 -0700

    removed commented code

--- src/redux/pokemonSlice.js
+++ src/redux/pokemonSlice.js
@@ -26,17 +26,3 @@ export const pokemonSlice = createSlice({
 
 export const { } = pokemonSlice.actions
 export default pokemonSlice.reducer
-
-// import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
-// import { Pokemon } from './types'
-
-// // Define a service using a base URL and expected endpoints
-// export const pokemonApi = createApi({
-//   reducerPath: 'pokemonApi',
-//   baseQuery: fetchBaseQuery({ baseUrl: 'https://pokeapi.co/api/v2/' }),
-//   endpoints: (builder) => ({
-//     getPokemonByName: builder.query<Pokemon, string>({
-//       query: (name) => `pokemon/${name}`,
-//     }),
-//   }),
-// })
